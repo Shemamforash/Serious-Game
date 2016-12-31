6a2a277e496311ab18f74ae9f9eacfb6c8693746
Shemamforash <samwest2@hotmail.co.uk>
2016-12-31 11:25:37 +0000

    Some Crap

---
+++ Assets/Scripts/FitToScreen.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FitToScreen : MonoBehaviour {
+	// Use this for initialization
+	void Start () {
+		SpriteRenderer spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
+		float spriteWidth = spriteRenderer.bounds.size.x;
+		float screenHeight = Camera.main.orthographicSize * 2;
+		float screenWidth = Screen.width / Screen.height * screenHeight;
+		float ratio = spriteWidth / screenWidth;
+		transform.localScale = Vector3.one * ratio;
+	}
+}
---
+++ Assets/StartGame.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class StartGame : MonoBehaviour {
+	public void LoadLevelSelect(){
+		SceneManager.LoadScene ("Level Select");
+	}
+}
