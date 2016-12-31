50086baed3c3222486f0b0052e47b21d1aa1340b
Shemamforash <samwest2@hotmail.co.uk>
2016-12-31 12:18:16 +0000

    Started Placing Map Points

--- Assets/Scripts/ScrollMap.cs
+++ Assets/Scripts/ScrollMap.cs
@@ -1,15 +1,31 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class ScrollMap : MonoBehaviour {
+public class ScrollMap : MonoBehaviour {	
+	private bool dragging = false, mouseSet = false;
+	private Vector3 mouseLast;
+	public float panSpeed = 3;
+	private float maxX, maxY, minX, minY;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
 	// Update is called once per frame
 	void Update () {
-	
+		if (dragging) {
+			Vector3 mouseCurrent = Input.mousePosition;
+			if (mouseSet != false) {
+				Vector3 vectDiff = Camera.main.ScreenToViewportPoint (mouseLast - mouseCurrent);
+				Camera.main.transform.Translate(vectDiff * panSpeed, Space.Self);
+			}
+			mouseSet = true;
+			mouseLast = mouseCurrent;
+		}
+	}
+		
+	void OnMouseDown(){
+		dragging = true;
+	}
+
+	void OnMouseUp(){
+		dragging = false;
+		mouseSet = false;
 	}
 }
