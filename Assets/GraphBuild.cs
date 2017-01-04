56235b9f456f0978990171403de32d3db8afcd62
Shemamforash <samwest2@hotmail.co.uk>
2017-01-04 16:14:41 +0000

    Didn't actually include the script as im a dumb

--- Assets/GraphBuild.cs
+++ Assets/GraphBuild.cs
@@ -1,15 +1,15 @@
 ﻿using UnityEngine;
-using System.Collections;
 using System.Collections.Generic;
 using System;
 using System.IO;
+using System.Linq;
 
 public class GraphBuild : MonoBehaviour {
 	private List<Node> map = new List<Node> ();
 	public GameObject lineObject;
     private List<Edge> edges = new List<Edge>();
     public GameObject start, end;
-    private Node startNode, endNode;
+    private Node startNode;
 
 	void Start () {
 		foreach (string line in File.ReadAllLines("./Assets/graph links.txt")) {
@@ -38,11 +38,9 @@ public class GraphBuild : MonoBehaviour {
 
     public void ToggleLine(GameObject start, GameObject end, bool active)
     {
-        foreach(Edge e in edges) {
-            if(e.Connects(start, end)) {
-                e.GetLine().SetActive(active);
-                break;
-            }
+        Edge e = FindEdge(start, end);
+        if(e != null) {
+            e.GetLine().SetActive(active);
         }
     }
 
@@ -51,35 +49,80 @@ public class GraphBuild : MonoBehaviour {
         foreach(Edge e in edges) {
             e.GetLine().SetActive(false);
         }
-        Queue<Node> q = new Queue<Node>();
+        List<Node> q = new List<Node>();
         Dictionary<Node, Node> path = new Dictionary<Node, Node>();
-        List<Node> visited = new List<Node>();
-        q.Enqueue(startNode);
+        Dictionary<Node, float> distances = new Dictionary<Node, float>();
         path.Add(startNode, null);
-        visited.Add(startNode);
+
+        foreach (Node n in map) {
+            distances.Add(n, 1000000f);
+            q.Add(n);
+        }
+
+        distances[startNode] = 0;
+
         while (q.Count > 0) {
-            Node current = q.Dequeue();
-            if(current.GetGameNode() == target) {
-                Node last = current;
-                path.TryGetValue(last, out current);
-                while (current != null) {
-                    ToggleLine(last.GetGameNode(), current.GetGameNode(), true);
-                    last = current;
-                    path.TryGetValue(last, out current);
+            //Node nearestNode = distances.OrderBy(pair => pair.Value).Take(1).First().Key;
+            Node nearestNode = null;
+            foreach(Node n in q) {
+                if(nearestNode == null) {
+                    nearestNode = n;
+                } else if(distances[n] < distances[nearestNode]) {
+                    nearestNode = n;
+                }
+            }
+            q.Remove(nearestNode);
+
+            if (nearestNode.GetGameNode() == target) {
+                Node last = nearestNode;
+                path.TryGetValue(last, out nearestNode);
+                while (nearestNode != null) {
+                    ToggleLine(last.GetGameNode(), nearestNode.GetGameNode(), true);
+                    last = nearestNode;
+                    path.TryGetValue(last, out nearestNode);
                 }
                 break;
             }
-            if (current == null) {
-                Debug.Log("No path available");
-            }
-            foreach (Node neighbor in current.GetNeighbors()) {
-                if (!visited.Contains(neighbor)) {
-                    visited.Add(neighbor);
-                    path.Add(neighbor, current);
-                    q.Enqueue(neighbor);
+
+            foreach (Node neighbor in nearestNode.GetNeighbors()) {
+                float newDistance = distances[nearestNode] + FindEdge(nearestNode.GetGameNode(), neighbor.GetGameNode()).GetDistance();
+                if (newDistance < distances[neighbor]) {
+                    distances[neighbor] = newDistance;
+                    path[neighbor] = nearestNode;
                 }
             }
         }
+
+        //Queue<Node> q = new Queue<Node>();
+        //Dictionary<Node, Node> path = new Dictionary<Node, Node>();
+        //List<Node> visited = new List<Node>();
+        //q.Enqueue(startNode);
+        //path.Add(startNode, null);
+        //visited.Add(startNode);
+
+        //while (q.Count > 0) {
+        //    Node current = q.Dequeue();
+        //    if (current.GetGameNode() == target) {
+        //        Node last = current;
+        //        path.TryGetValue(last, out current);
+        //        while (current != null) {
+        //            ToggleLine(last.GetGameNode(), current.GetGameNode(), true);
+        //            last = current;
+        //            path.TryGetValue(last, out current);
+        //        }
+        //        break;
+        //    }
+        //    if (current == null) {
+        //        Debug.Log("No path available");
+        //    }
+        //    foreach (Node neighbor in current.GetNeighbors()) {
+        //        if (!visited.Contains(neighbor)) {
+        //            visited.Add(neighbor);
+        //            path.Add(neighbor, current);
+        //            q.Enqueue(neighbor);
+        //        }
+        //    }
+        //}
     }
 
     private void Update()
@@ -98,16 +141,25 @@ public class GraphBuild : MonoBehaviour {
         Node newNode = new Node(val);
         if(newNode.GetGameNode() == start) {
             startNode = newNode;
-        } else if(newNode.GetGameNode() == end) {
-            endNode = newNode;
         }
         map.Add(newNode);
 		return newNode;
 	}
 
+    private Edge FindEdge(GameObject a, GameObject b)
+    {
+        foreach(Edge e in edges) {
+            if(e.Connects(a, b)) {
+                return e;
+            }
+        }
+        return null;
+    }
+
     private class Edge
     {
         private GameObject line, start, end;
+        private float distance;
 
         public Edge(GameObject line, GameObject start, GameObject end)
         {
@@ -118,6 +170,11 @@ public class GraphBuild : MonoBehaviour {
             position.z = 0;
             line.transform.position = position;
             line.transform.SetParent(GameObject.Find("Map").transform);
+            float xDiff = start.transform.position.x - end.transform.position.x;
+            xDiff = xDiff * xDiff;
+            float yDiff = start.transform.position.y - end.transform.position.y;
+            yDiff = yDiff * yDiff;
+            this.distance = (float)Math.Sqrt(xDiff + yDiff);
         }
 
         public void AlignLineRenderer()
@@ -136,6 +193,11 @@ public class GraphBuild : MonoBehaviour {
         public GameObject GetLine()
         {
             return line;
+        }
+
+        public float GetDistance()
+        {
+            return distance;
         }
     }
 
@@ -157,7 +219,15 @@ public class GraphBuild : MonoBehaviour {
 			if (!neighbors.Contains (n)) {
 				neighbors.Add (n);
 			}
-		}
+            Color spriteColor = gameNode.GetComponent<SpriteRenderer>().color;
+            //if(neighbors.Count > 1) {
+            //    spriteColor.a = 1f;
+            //} else {
+            //    spriteColor.a = 0f;
+            //}
+            gameNode.GetComponent<SpriteRenderer>().color = spriteColor;
+
+        }
 
 		public List<Node> GetNeighbors(){
 			return neighbors;
