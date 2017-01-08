c5c83793779cc30d34cd2b26fd2a166361ca36a4
iovana <ip4g14@soton.ac.uk>
2017-01-08 12:10:06 +0000

    Migrate to new laptop woooo

---
+++ Assets/Scripts/RotationBehaviour.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class RotationBehaviour : MonoBehaviour {
+
+    public GameObject pointA, pointB, directionHousing, compassHousing;
+
+    void Start () {
+	
+	}
+
+    float CalculateAngle ()
+    {
+        float x1 = pointA.transform.position.x;
+        float x2 = pointB.transform.position.x;
+        float y1 = pointA.transform.position.y;
+        float y2 = pointB.transform.position.y;
+
+        double angleRad = Math.Atan2(y2, x2) - Math.Atan2(y1, x2);
+        float deg = (float) angleRad * Mathf.Rad2Deg;
+
+        return (float) deg;
+    }
+	
+	void Update () {
+        float rotation = directionHousing.transform.rotation.z;
+        float angleDiff = rotation - CalculateAngle();
+        Debug.Log(CalculateAngle() + "     " + rotation + "    " + angleDiff);
+	}
+}
