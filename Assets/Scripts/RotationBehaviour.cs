dab1c1056d4c5a1491204b596f68c2a343c6027f
iovana <ip4g14@soton.ac.uk>
2017-01-09 16:47:53 +0000

    angle

--- Assets/Scripts/RotationBehaviour.cs
+++ Assets/Scripts/RotationBehaviour.cs
@@ -9,23 +9,25 @@ public class RotationBehaviour : MonoBehaviour {
     void Start () {
 	
 	}
-
-    float CalculateAngle ()
-    {
-        float x1 = pointA.transform.position.x;
-        float x2 = pointB.transform.position.x;
-        float y1 = pointA.transform.position.y;
-        float y2 = pointB.transform.position.y;
-
-        double angleRad = Math.Atan2(y2, x2) - Math.Atan2(y1, x2);
-        float deg = (float) angleRad * Mathf.Rad2Deg;
-
-        return (float) deg;
-    }
 	
 	void Update () {
-        float rotation = directionHousing.transform.rotation.z;
-        float angleDiff = rotation - CalculateAngle();
-        Debug.Log(CalculateAngle() + "     " + rotation + "    " + angleDiff);
+        //This is the angle between the North gridline and the line between A and B
+        float xDifference = pointB.transform.position.x - pointA.transform.position.x;
+        float yDifference = pointB.transform.position.y - pointA.transform.position.y;
+        Vector2 directionVector = new Vector2(xDifference, yDifference);
+        directionVector.Normalize();
+        //float directionRotation = CalculateAngleBetweenLineAndUp(directionVector);
+
+        float y = Mathf.Deg2Rad * directionHousing.transform.rotation.eulerAngles.z; //radians
+        if(y < 0){
+            float complement = -((float)Math.PI / 2) - y;
+            y = (float)Math.PI / 2 - complement;
+        }
+        y = (float)Math.PI * 2 - y;
+
+        Vector2 housingVector = new Vector2();
+        housingVector.x = (float)Math.Sin(y);
+        housingVector.y = (float)Math.Cos(y);
+        float angleDiff = (float)Vector2.Angle(directionVector, housingVector);
 	}
 }
