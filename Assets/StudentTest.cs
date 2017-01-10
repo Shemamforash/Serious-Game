5c6a99cdcb01fe0ebb15c17885a81e7197865f67
Shemamforash <samwest2@hotmail.co.uk>
2017-01-10 12:08:44 +0000

    Done the student test

--- Assets/StudentTest.cs
+++ Assets/StudentTest.cs
@@ -1,5 +1,4 @@
 ﻿using UnityEngine;
-using System.Collections;
 using System.IO;
 using System.Collections.Generic;
 using UnityEngine.UI;
@@ -9,8 +8,13 @@ public class StudentTest : MonoBehaviour {
 	private List<Question> questions = new List<Question>();
 	private int questionIterator = 0;
 	// Use this for initialization
-	public GameObject questionObject, answer1Object, answer2Object, answer3Object, toggleGroup;
+	public GameObject questionObject, answer1Object, answer2Object, answer3Object, toggleGroup, testContainer, scoreContainer;
+	private int totalScore, playerScore = 0;
+
+	private bool reviseCompass = false, reviseRoute = false;
 	void Start () {
+		testContainer.SetActive(true);
+		scoreContainer.SetActive(false);
 		int ctr = 0;
 		int qNo = -1;
 		string questionString = "", answer1 = "", answer2 = "", answer3 = "";
@@ -26,11 +30,16 @@ public class StudentTest : MonoBehaviour {
 				answer2 = line.Remove(0, 3);
 			}else if(ctr == 4){
 				answer3 = line.Remove(0, 3);
-				questions.Add(new Question(qNo, questionString, answer1, answer2, answer3));
+				bool aboutCompass = true;
+				if(qNo > 4){
+					aboutCompass = false;
+				}
+				questions.Add(new Question(qNo, questionString, answer1, answer2, answer3, aboutCompass));
 				ctr = -1;
 			}
 			++ctr;
 		}
+		totalScore = questions.Count;
 		NextQuestion();
 	}
 	
@@ -39,34 +48,69 @@ public class StudentTest : MonoBehaviour {
 		Toggle currentToggle = toggleGroup.GetComponent<ToggleGroup>().ActiveToggles().FirstOrDefault();
 		if(currentToggle != null){
 			GameObject toggleObject = currentToggle.gameObject;
-			if(toggleObject == answer1Object){
-
-			} else if(toggleObject == answer2Object){
-
-			} else if(toggleObject == answer3Object){
-
+			string text = toggleObject.transform.Find("Text").GetComponent<Text>().text;
+			if(questions[questionIterator - 1].CheckAnswer(text)){
+				++playerScore;
 			}
+			else {
+				if(questions[questionIterator - 1].IsAboutCompass()){
+					reviseCompass = true;
+				} else {
+					reviseRoute = true;
+				}
+			}
+			Debug.Log(playerScore + "/" + totalScore);
 			NextQuestion();
 		}
 	}
 
+	void FinishTest(){
+		testContainer.SetActive(false);
+		scoreContainer.SetActive(true);
+		string suggestion = "";
+		if(reviseCompass) {
+			suggestion = "compass orientation";
+		}
+		if(reviseRoute) {
+			if(reviseCompass){
+				suggestion += " and ";
+			}
+			suggestion += "route finding";
+		}
+		if(suggestion != ""){
+			suggestion = "You could revise " + suggestion + ", then return and improve your score!";
+		}
+		Debug.Log(scoreContainer.transform.Find("Text"));
+		scoreContainer.transform.Find("Text").GetComponent<Text>().text = "You finished the test!\n" +
+			"Final Score: " + playerScore + "/" + totalScore + "\n\n" +
+			suggestion;
+	}
+
 	void NextQuestion(){
-		questionObject.transform.Find("Text").GetComponent<Text>().text = questions[questionIterator].GetQuestion();
-		answer1Object.transform.Find("Text").GetComponent<Text>().text = questions[questionIterator].GetAnswer1();
-		answer2Object.transform.Find("Text").GetComponent<Text>().text = questions[questionIterator].GetAnswer2();
-		answer3Object.transform.Find("Text").GetComponent<Text>().text = questions[questionIterator].GetAnswer3();
-		questionIterator++;
+		if(questionIterator < totalScore){
+			questionObject.transform.Find("Text").GetComponent<Text>().text = questions[questionIterator].GetQuestion();
+			List<Answer> answers = questions[questionIterator].GetAnswers();
+			answer1Object.transform.Find("Text").GetComponent<Text>().text = answers[0].GetAnswer();
+			answer2Object.transform.Find("Text").GetComponent<Text>().text = answers[1].GetAnswer();
+			answer3Object.transform.Find("Text").GetComponent<Text>().text = answers[2].GetAnswer();
+			questionIterator++;
+		} else {
+			FinishTest();
+		}
 	}
 
 	private class Question{
 		private int no, correctAnswer;
-		private string questionText, answer1, answer2, answer3;
-		public Question(int no, string questionText, string answer1, string answer2, string answer3){
+		private string questionText;
+		private List<Answer> answers = new List<Answer>();
+		private bool aboutCompass;
+		public Question(int no, string questionText, string answer1, string answer2, string answer3, bool aboutCompass){
 			this.no = no;
 			this.questionText = questionText;
-			this.answer1 = answer1;
-			this.answer2 = answer2;
-			this.answer3 = answer3;
+			answers.Add(new Answer(answer1, true));
+			answers.Add(new Answer(answer2, false));
+			answers.Add(new Answer(answer3, false));
+			this.aboutCompass = aboutCompass;
 		}
 
 		public int GetNo(){
@@ -76,16 +120,48 @@ public class StudentTest : MonoBehaviour {
 			return questionText;
 		}
 
-		public string GetAnswer1(){
-			return answer1;
-		}
-		
-		public string GetAnswer2(){
-			return answer2;
+		public List<Answer> GetAnswers(){
+			int swaps = 3;
+			for(int i = 0; i < swaps; ++i){
+				List<int> possible = new List<int>(){0, 1, 2};
+				int a = possible[Random.Range(0, possible.Count)];
+				possible.Remove(a);
+				int b = possible[Random.Range(0, possible.Count)];
+				Answer temp = answers[a];
+				answers[a] = answers[b];
+				answers[b] = temp;
+			}
+			return answers;
 		}
 
-		public string GetAnswer3(){
-			return answer3;
+		public bool CheckAnswer(string givenAnswer){
+			foreach(Answer answer in answers){
+				if(answer.GetAnswer() == givenAnswer){
+					return answer.IsCorrect();
+				}
+			}
+			return false;
+		}
+
+		public bool IsAboutCompass(){
+			return aboutCompass;
 		}
 	}
+
+	private class Answer{
+			private string answer;
+			private bool correct;
+			public Answer(string answer, bool correct){
+				this.answer = answer;
+				this.correct = correct;
+			}
+
+			public bool IsCorrect(){
+				return correct;
+			}
+
+			public string GetAnswer(){
+				return answer;
+			}
+		}
 }
