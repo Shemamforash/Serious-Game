48078ed4d3161198c3bbf3c6b3787bd98336da52
iovana <ip4g14@soton.ac.uk>
2017-01-11 17:59:36 +0000

    Working Compass

--- Assets/Scripts/KeyboardMovement.cs
+++ Assets/Scripts/KeyboardMovement.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class KeyboardMovement : MonoBehaviour
 {
     public float moveSpeed = 2.5f, rotationSpeed = 1.5f;
+    private int step = 0;
 
     public GameObject directionHousing, compassHousing, compassNeedle;
     private GameObject currentObject;
@@ -12,69 +13,46 @@ public class KeyboardMovement : MonoBehaviour
 
     private bool movementLocked = false; //if movement locked is false directionHousing is the current object then movement is allowed, if movement locked true opposite is true
 
-    void Start()
-    {
+    void Start() {
         currentObject = directionHousing;
         Vector3 tempRotation = new Vector3(0, 0, Random.Range(0, 360));
         needleRotation = Quaternion.Euler(tempRotation);
         rotationScript = gameObject.GetComponent<RotationBehaviour>();
     }
 
-    void PointNorth()
-    {
+    void PointNorth() {
         //compass needle points anywhere at the beggining
         compassNeedle.transform.rotation = needleRotation;
     }
 
     // move by using WASD/arrow keys and rotate
-    void Update()
-    {
-        Vector2 diff = new Vector2(0, 0);
-        float offset = moveSpeed * Time.deltaTime;
-
-        if (Input.GetKey(KeyCode.Space))
-        {
-            movementLocked = !movementLocked;
-        }
-
-        //move between compass components when player presses Space
-        if (movementLocked)
-        {
-            currentObject = compassHousing;
-        }
-        else
-        {
-            currentObject = directionHousing;
-        }
-
-        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
-        {
-            diff.y += offset;
-        }
-        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
-        {
-            diff.x -= offset;
-        }
-        if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
-        {
-            diff.y -= offset;
-        }
-        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
-        {
-            diff.x += offset;
-        }
+    void Update() {
         // check if movement is unlocked
         if (!movementLocked)
         {
+            Vector2 diff = new Vector2(0, 0);
+            float offset = moveSpeed * Time.deltaTime;
+
+            if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
+            {
+                diff.y += offset;
+            }
+            if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+            {
+                diff.x -= offset;
+            }
+            if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
+            {
+                diff.y -= offset;
+            }
+            if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+            {
+                diff.x += offset;
+            }
             Vector2 currentPosition = transform.position;
             currentPosition.x = currentPosition.x + diff.x;
             currentPosition.y = currentPosition.y + diff.y;
             currentObject.transform.position = currentPosition;
-            rotationScript.SetCurrentObject(directionHousing);
-            rotationScript.DefaultVector();
-        } else {
-            rotationScript.SetCurrentObject(compassHousing);
-            rotationScript.SetVector(new Vector2(0, 1));
         }
 
         // rotation is always allowed
@@ -91,5 +69,34 @@ public class KeyboardMovement : MonoBehaviour
 
         // move compass needle
         PointNorth();
+    }
+
+    //on confirm call method to calculate score from direction housing angle and a->b vector
+    //on second confirm call method "" "" "" from compass housing and Vector2.Up
+    //on third confirm """"" using compass housing rotation and compass needle rotation
+    //increment the step counter at the END of the method
+
+    public void ConfirmSelection() {
+        //step logic for changing current vector and object
+        if(step == 0){
+            float angle = directionHousing.transform.rotation.eulerAngles.z;
+            float score1 = rotationScript.ScoreBetweenVectorAndAngle(angle, rotationScript.DefaultVector());
+            Debug.Log(score1);
+            movementLocked = true;
+            currentObject = compassHousing;
+        }
+        else if(step == 1) {
+            float angle = compassHousing.transform.rotation.eulerAngles.z;
+            float score2 = rotationScript.ScoreBetweenVectorAndAngle(angle, new Vector2(0, 1));
+            Debug.Log(score2);
+            currentObject = directionHousing;
+        } else if (step == 2) {
+            float angleA = compassNeedle.transform.rotation.eulerAngles.z;
+            float angleB = compassHousing.transform.rotation.eulerAngles.z;
+            float score1 = rotationScript.ScoreBetweenAngleAndAngle(angleA, angleB);
+            Debug.Log(score1);
+        }
+        ++step;
+
     }
 }
--- Assets/Scripts/RotationBehaviour.cs
+++ Assets/Scripts/RotationBehaviour.cs
@@ -6,8 +6,7 @@ using System.Collections.Generic;
 public class RotationBehaviour : MonoBehaviour {
 
     public GameObject directionHousing, compassHousing;
-    private GameObject currentObject, pointA, pointB;
-    public Vector2 vector;
+    private GameObject pointA, pointB;
 
     void Start () {
         List<GameObject> points = new List<GameObject>(GameObject.FindGameObjectsWithTag("Point"));
@@ -23,19 +22,17 @@ public class RotationBehaviour : MonoBehaviour {
         foreach(GameObject point in points) {
             point.SetActive(false);
         }
-        DefaultVector();
-        currentObject = directionHousing;
 	}
 	
-    public void DefaultVector() {
+    public Vector2 DefaultVector() {
         float xDifference = pointB.transform.position.x - pointA.transform.position.x;
         float yDifference = pointB.transform.position.y - pointA.transform.position.y;
         Vector2 directionVector = new Vector2(xDifference, yDifference);
         directionVector.Normalize();
-        vector = directionVector;
+        return directionVector;
     }
 
-    public int ScoreBetweenVectorAndAngle(float angle){
+    private Vector2 ZAngleToVector(float angle) {
         angle = Mathf.Deg2Rad * angle; //radians
 
         if(angle < 0){
@@ -47,7 +44,13 @@ public class RotationBehaviour : MonoBehaviour {
         Vector2 angleVector = new Vector2();
         angleVector.x = (float)Math.Sin(angle);
         angleVector.y = (float)Math.Cos(angle);
-        float angleDiff = (float)Vector2.Angle(vector, angleVector);
+        return angleVector;
+    }
+
+     public int ScoreBetweenAngleAndAngle(float angleA, float angleB){
+        Vector2 vectorA = ZAngleToVector(angleA);
+        Vector2 vectorB = ZAngleToVector(angleB);        
+        float angleDiff = (float)Vector2.Angle(vectorA, vectorB);
         double score1 = 0;
         // now for the scoring
         // full points if the angle is between 0 and 10
@@ -59,15 +62,17 @@ public class RotationBehaviour : MonoBehaviour {
         return (int)Math.Floor(score1 * 100);
     }
 
-	void Update () {       
-        Debug.Log(ScoreBetweenVectorAndAngle(currentObject.transform.rotation.eulerAngles.z));
-	}
-
-    public void SetVector(Vector2 vector){
-        this.vector = vector;
-    }
-
-    public void SetCurrentObject(GameObject currentObject){
-        this.currentObject = currentObject;
+    public int ScoreBetweenVectorAndAngle(float angle, Vector2 vector){
+        Vector2 angleVector = ZAngleToVector(angle);
+        float angleDiff = (float)Vector2.Angle(vector, angleVector);
+        double score1 = 0;
+        // now for the scoring
+        // full points if the angle is between 0 and 10
+        if(angleDiff < 10) {
+            score1 = 1;
+        } else {
+            score1 = 3 * (Math.Pow(angleDiff - 2, -0.2)) - 1;
+        }
+        return (int)Math.Floor(score1 * 100);
     }
 }
