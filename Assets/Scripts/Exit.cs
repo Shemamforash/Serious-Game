00f28f20cfb52554cbe4970cca834733fa275f57
Shemamforash <samwest2@hotmail.co.uk>
2017-01-12 09:59:43 +0000

    Added route help

--- Assets/Scripts/Exit.cs
+++ Assets/Scripts/Exit.cs
@@ -2,11 +2,17 @@
 using UnityEngine.SceneManagement;
 
 public class Exit : MonoBehaviour {
+	public GameObject helpObject;
+
 	public void BackToMenu(){
 		SceneManager.LoadScene("Main Menu");
 	}
 
 	public void BackToLevelSelect(){
 		SceneManager.LoadScene("Level Select");
+	}
+
+	public void ToggleHelp(){
+		helpObject.SetActive(!helpObject.activeInHierarchy);
 	}
 }
--- Assets/Scripts/RouteGuideBehaviour.cs
+++ Assets/Scripts/RouteGuideBehaviour.cs
@@ -7,9 +7,19 @@ using UnityEngine.SceneManagement;
 public class RouteGuideBehaviour : MonoBehaviour {
 	public List<Sprite> steps = new List<Sprite>();
 	// Use this for initialization
+	private List<string> instructions = new List<string>();
 	private int currentStep = 0;
 	public GameObject stepImage, stepInstructions;
 	void Start () {
+		instructions.Add("The first step in plotting a route is to find your start point (shown in green). Now you need to look at the possible routes from this point to your goal. " +
+		"Here we have identified 2 potential points (in blue) to start off your route.");
+		instructions.Add("We decided to go with the highlighted route as it's shorter and should be easier to follow. " +
+		"You can decide whether a route is easy to follow based on the paths and roads on the map. " + 
+		"Now we continue plotting a route from the new point towards the destination");
+		instructions.Add("Eventually we will have plotted a complete route to the destination. " +
+		"It's important to consider the length and the potential dangers of a route during planning.");
+		instructions.Add("Danger's can often be identified from the map symbols. " + 
+		"Here for example we have highlighted the dangers of steep climbs, major roads, and natural dangers like caves");
 		NextStep();
 	}
 
@@ -18,6 +28,7 @@ public class RouteGuideBehaviour : MonoBehaviour {
 			SceneManager.LoadScene("Route Challenge");
 		}
 		stepImage.GetComponent<Image>().sprite = steps[currentStep];
+		stepInstructions.GetComponent<Text>().text = instructions[currentStep];
 		++currentStep;
 	}
 }
